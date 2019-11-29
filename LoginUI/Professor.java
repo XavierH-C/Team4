314fca8a20eed1ae15f0e430dd02ff084c4c00f9
reidhurlburt <55860310+reidhurlburt@users.noreply.github.com>
2019-11-28 21:25:40 -0400

    Create Professor.java

---
+++ LoginUI/Professor.java
@@ -0,0 +1,8 @@
+public class Professor {
+	public String professorId;
+	public String password;
+	public String firstName;
+	public String lastName; 
+	public String address;
+	public String contactInfo;
+}
