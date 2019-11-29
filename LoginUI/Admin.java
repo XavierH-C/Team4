4564357978a9d3c123ff4038d3287f46dacffb8b
reidhurlburt <55860310+reidhurlburt@users.noreply.github.com>
2019-11-28 21:25:13 -0400

    Create Admin.java

---
+++ LoginUI/Admin.java
@@ -0,0 +1,8 @@
+public class Admin {
+	public String adminId;
+	public String password;
+	public String firstName;
+	public String lastName; 
+	public String address;
+	public String contactInfo;
+}
