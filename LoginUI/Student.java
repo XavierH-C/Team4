043ef4d6b6eb627c61b02688eac3eba4a4fcdd04
reidhurlburt <55860310+reidhurlburt@users.noreply.github.com>
2019-11-28 21:26:29 -0400

    Create Student.java

---
+++ LoginUI/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+	public String studentId;
+	public String password;
+	public String firstName;
+	public String lastName; 
+	public String address;
+	public String contactInfo;
+}
