470ba1ce341d82d447d87a875d3b74eb11a89c33
reidhurlburt <55860310+reidhurlburt@users.noreply.github.com>
2019-11-28 21:26:07 -0400

    Create TeachingAssistant.java

---
+++ LoginUI/TeachingAssistant.java
@@ -0,0 +1,8 @@
+public class TeachingAssistant {
+	public String teachingAssistantId;
+	public String password;
+	public String firstName;
+	public String lastName; 
+	public String address;
+	public String contactInfo;
+}
